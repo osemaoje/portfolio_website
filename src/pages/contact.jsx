5dce4f68bfeaa3e317f4203d1e7268c95cfafac4
Osemaoje Elimimian <oje.elimimian@alphamorganbank.com>
2026-05-15 11:53:49 +0100

    Edited the Contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -48,9 +48,14 @@ const Contact = () => {
 							me. I welcome your feedback, questions, and
 							suggestions. If you have a specific question or
 							comment, please feel free to email me directly at
-							&nbsp;{" "}
+							&nbsp;
 							<a href={`mailto:${INFO.main.email}`}>
 								{INFO.main.email}
+							</a>
+							. You can also reach me via phone at
+							&nbsp;
+							<a href="tel:+2348123456789">
+								+234 816 190 8359
 							</a>
 							. I make an effort to respond to all messages within
 							24 hours, although it may take me longer during busy
@@ -58,13 +63,14 @@ const Contact = () => {
 							on my website to get in touch. Simply fill out the
 							required fields and I'll get back to you as soon as
 							possible. Finally, if you prefer to connect on
-							social media, you can find me on{" "}
+							social media, you can find me on
+							&nbsp;
 							<a
 								href={INFO.socials.linkedin}
 								target="_blank"
 								rel="noreferrer"
 							>
-								{INFO.socials.linkedin}
+								LinkedIn
 							</a>
 							. I post regular updates and engage with my
 							followers there, so don't hesitate to reach out.
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -48,12 +48,8 @@ const Projects = () => {
 							and I'm proud of the progress I've made. Many of
 							these projects are open-source and available for
 							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							interested in any of the projects you can view these
+							projects below.
 						</div>
 
 						<div className="projects-list">
